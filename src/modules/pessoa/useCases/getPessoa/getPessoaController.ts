e8285737bf59378f2341f6b08538f79a4c4f0882
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2022-09-23 13:23:09 -0300

    fixing case sensitive

--- src/modules/pessoa/useCases/getPessoa/getPessoaController.ts
+++ src/modules/pessoa/useCases/getPessoa/getPessoaController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { AppError } from "../../../../errors/AppError";
 import { GetPessoaDTO } from "../dtos";
-import { GetPessoaUseCase } from "./getPessoaUseCase";
+import { GetPessoaUseCase } from "./changeUseCase";
 
 export class GetPessoasController {
   async handle(req: Request, res: Response) {
